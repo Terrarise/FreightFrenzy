7d028d4820608db20e492c18bcabbbb621f7c0bb
nithinnnnn <nithinlokesh125@gmail.com>
2021-12-19 18:03:53 -0800

    a lot of changes yes

--- TeamCode/src/main/java/auton/Auto.java
+++ TeamCode/src/main/java/auton/Auto.java
@@ -8,5 +8,5 @@ import static global.General.*;
 public abstract class Auto extends LinearOpMode implements Common {
     // Base class for autonomous classes
     // Uses the generator to create the path
-    // Uses the executor to move the robot along the path (and any decisions that need to be made while the robot is moving)
+    // Uses the execu tor to move the robot along the path (and any decisions that need to be made while the robot is moving)
 }
